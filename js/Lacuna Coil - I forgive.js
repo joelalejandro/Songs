897b0e1c7720e812a42976bc5e7ac44fa9afabf7
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2014-10-21 15:00:26 -0300

    Create Lacuna Coil - I forgive.js

---
+++ js/Lacuna Coil - I forgive.js
@@ -0,0 +1,86 @@
+// I forgive (but I won't forget your name)
+// Lacuna Coil
+
+if (you.knowThat(I.haveBeenHurt)) {    // You know I've been hurt before
+}                                      // But you didn't really care
+me.emptyWordsCount = emptyWordsLimit;  // Had enough of empty words
+me.stay = false;                       // I'm not here to stay
+
+I.set("support", "you", false);        // I cannot deal with you anymore
+you.timesLineCrossed++;                // Once again you've crossed the line
+you.incrementTimesUnderstimated(me)    // And you're underestimating me again
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // I forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+I.promise(I.see(you, "pay").reason(
+  you.haveDone("damage").all()         // All the damage that you've done
+);                                     // I want to see you pay
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // 'Cause I will forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+you.pay({ later: true }).reason(
+  you.haveDone().all()                 // You will pay for all that you've done
+);
+
+I.put({ what: "trust", where: "you" })
+.then(function() {                     // When I put my trust in you
+  you.throw("trust");                  // You just threw it all away
+  you.put({ what: "pain", where: "me"})// All the pain you put me through
+});
+
+you.stay = false;                      // You're not here to stay
+
+you.incrementTimesUnderstimated(me)    // And you're underestimating me again
+you.get({ what: you.given.all() });    // You'll get what you've been giving
+
+rain(); rain();                        // Let it rain, let it rain
+rain({ where: me.heart });             // Let it rain into my heart
+I.wash({ what: "stains", from: you }); // I'll wash away the stains
+                                       // That you left behind
+
+rain(); rain();                        // Let it rain, let it rain
+rain({ where: me.heart });             // Let it rain into my heart
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // I forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+I.promise(I.see(you, "pay").reason(
+  you.haveDone("damage").all()         // All the damage that you've done
+);                                     // I want to see you pay
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // 'Cause I will forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+you.pay({ later: true }).reason(
+  you.haveDone().all()                 // You will pay for all that you've done
+);
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // I forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+I.promise(I.see(you, "pay").reason(
+  you.haveDone().all()                 // You will pay for all that you've done
+);                                     // I want to see you pay
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // 'Cause I will forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+you.pay({ later: true }).reason(
+  you.haveDone().all()                 // You will pay for all that you've done
+);
+
+I.forgive(people.filter({ function(p) {
+  return I.hurtedBy().indexOf(p) > -1;
+}));                                   // 'Cause I will forgive the ones who hurt me
+I.remember(you.name);                  // But I'll not forget your name
+you.pay({ later: true }).reason(
+  you.haveDone().all()                 // You will pay for all that you've done
+);
