f4189ed65932841f32d9439f1fdeceed19361f69
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2014-10-27 13:23:47 -0300

    Create Madonna - 4 minutes.js

---
+++ js/Madonna - 4 minutes.js
@@ -0,0 +1,224 @@
+// 4 minutes
+// Madonna, Justin Timberlake, Timbaland
+
+with (timbaland) {
+  rap(function() {
+    setTimeout(function() {
+      shout("I'm outta time");
+    }, 60 * 4);                   // I'm outta time and all I got is 4 minutes
+    frickiFrick();                // Fricki fricki
+    timeRemaining = 60 * 4;       // 4 minutes eh
+  }).times(8);
+  laugh();                        // Ha ha
+  acceptInformally();             // Yeah
+  laugh();                        // Ha ha
+  announceBreakdown();            // Breakdown come on
+};
+
+with (justin) {
+  saluteInformally();             // Hey
+  doDancebreakSound();            // Uh
+  announce(madonna);              // Come on, Madonna
+}
+
+with (madonna) {
+  wait(boy).to(function() {       // Come on boy I've been waiting
+    stroll.pickUp();              // for somebody to pick up my stroll
+  })
+}
+
+with (justin) {
+  now()                           // Now don't waste time,
+    .require("desire")            // give me desire,
+    .require("roll", {            // tell me how you wanna roll
+      data: "how" 
+    });
+}
+
+with (madonna) {
+  require("speeder")              // I want somebody to speed it up for me then take it down slow
+    .then()
+    .require("slow");
+  assert.isTrue(                  // There's enough room for both
+    spaceAvailable 
+      >= spaceRequired
+  );
+}
+
+
+with (justin) {
+  // Girl I can hit you back just gotta show me where it's at
+  if (girl.hasShowed("back", justin)) {
+    can("hitBack", girl)
+    // Are you ready to go? (Are you ready to go?)
+    askIfReady();
+    askIfReady();
+  };
+}
+
+[madonna, justin].forEach(function() {
+  you.get(it);
+  if (you.want(it)) {       // If you want it
+    warn(ALREADY_GOT);      // You've already got it
+  };
+  you.think(it);
+  assert.ok(  
+    you.lastThought == you.lastWish   // If you thought it
+  );                                  // It better be what you want
+  you.feel(it);
+  if (you.feel(it)) {                                 // If you feel it
+    assert.ok(you.lastFeeling instanceof Real);       // It must be real just
+    say(word).then(function() {                       // Say the word and
+      you.get(you.lastWish);                          // I'm gonna give you what you want
+    });
+  };
+});
+
+
+with (madonna) {
+  announce(time.wait());    // Time is waiting
+}
+
+with (justin) {
+  // We only got four minutes to save the world
+  announce(timeRemaining).while(!world.saved);
+}
+
+assert.true(madonna.isSure);  // No hesitating
+madonna.grab(boy);            // Grab a boy
+justin.grab(girl);            // Go grab your girl
+
+with (madonna) {
+  announce(time.wait());    // Time is waiting
+}
+
+with (justin) {
+  // We only got four minutes to save the world
+  announce(timeRemaining).while(!world.saved);
+}
+
+assert.true(madonna.isSure);  // No hesitating
+
+justin.announce(timeRemaining)
+.while(!world.saved).with("huh"); // We only got four minutes huh four minutes
+
+justin.order(all, function(p) { p.keep(it).up(); });          // So keep it up keep it up
+assert.trueForAll(function(p) { return !p.isPrimaDonna; });   // Don't be a prima donna
+
+madonna.order(all, function(p) {
+  p.get("heart").giveTo("them");    // You gotta get em a heart
+})
+// Tick tock tick tock tick tock
+madonna.metamorphInto("clock").tick().tock().tick().tock().tick().tock();
+
+assert.isTrue(justin.isRight);  // That's right
+justin.order(all, function(p) { p.keep(it).up(); });          // keep it up keep it up
+assert.trueForAll(function(p) { return !p.isPrimaDonna; });   // Don't be a prima donna
+
+madonna.order(all, function(p) {
+  p.get("heart").giveTo("them");    // You gotta get em a heart
+})
+// Tick tock tick tock tick tock
+madonna.metamorphInto("clock").tick().tock().tick().tock().tick().tock();
+
+madonna.think(function() {
+  require("intervention");    // Sometimes I think what I need is a you intervention
+}).random(0, 1);
+
+justin.acceptInformally();
+
+justin.know(function() {
+  assert.isTrue(you.like(it));   // And I know I can tell that you like it and that it's good
+  check(you.getMovement());      // By the way that you move
+  return IT_IS_GOOD;
+});
+
+justin.vocalize("oh").saluteInformally(); // Oh hey
+
+// The road to hell is paved with good intentions
+madonna.announces(new RoadToHell({ inherits: new ListOf(GoodIntention) }));
+madonna.acceptInformally();     // Yeah
+
+[Justin:]
+if (justin.isDead({ when: "tonight" })) {
+  if (justin.lastAction == justin.lastWish) {
+    justin.announce("I did what I wanted to do");   // But if I die tonight at least I can say I did what I wanted to do
+    justin.defy(you);                               // Tell me how 'bout you?
+  }
+}
+
+[madonna, justin].forEach(function() {
+  you.get(it);
+  if (you.want(it)) {       // If you want it
+    warn(ALREADY_GOT);      // You've already got it
+  };
+  you.think(it);
+  assert.ok(  
+    you.lastThought == you.lastWish   // If you thought it
+  );                                  // It better be what you want
+  you.feel(it);
+  if (you.feel(it)) {                                 // If you feel it
+    assert.ok(you.lastFeeling instanceof Real);       // It must be real just
+    say(word).then(function() {                       // Say the word and
+      you.get(you.lastWish);                          // I'm gonna give you what you want
+    });
+  };
+});
+
+with (madonna) {
+  announce(time.wait());    // Time is waiting
+}
+
+with (justin) {
+  // We only got four minutes to save the world
+  announce(timeRemaining).while(!world.saved);
+}
+
+assert.true(madonna.isSure);  // No hesitating
+madonna.grab(boy);            // Grab a boy
+justin.grab(girl);            // Go grab your girl
+
+
+with (madonna) {
+  announce(time.wait());    // Time is waiting
+}
+
+with (justin) {
+  // We only got four minutes to save the world
+  announce(timeRemaining).while(!world.saved);
+}
+
+assert.true(madonna.isSure);  // No hesitating
+
+justin.announce(timeRemaining)
+.while(!world.saved).with("huh"); // We only got four minutes huh four minutes
+
+justin.order(all, function(p) { p.keep(it).up(); });          // So keep it up keep it up
+assert.trueForAll(function(p) { return !p.isPrimaDonna; });   // Don't be a prima donna
+
+madonna.order(all, function(p) {
+  p.get("heart").giveTo("them");    // You gotta get em a heart
+})
+// Tick tock tick tock tick tock
+madonna.metamorphInto("clock").tick().tock().tick().tock().tick().tock();
+
+assert.isTrue(justin.isRight);  // That's right
+justin.order(all, function(p) { p.keep(it).up(); });          // keep it up keep it up
+assert.trueForAll(function(p) { return !p.isPrimaDonna; });   // Don't be a prima donna
+
+madonna.order(all, function(p) {
+  p.get("heart").giveTo("them");    // You gotta get em a heart
+})
+// Tick tock tick tock tick tock
+madonna.metamorphInto("clock").tick().tock().tick().tock().tick().tock();
+
+timbaland.announce(breakdown).acceptInformally(); // Breakdown, yeah
+
+madonna.tick().tock().tick().tock().tick().tock(); // Tick tock tick tock tick tock
+
+timbaland.acceptInformally().vocalize("uh");
+
+madonna.tick().tock().tick().tock().tick().tock(); // Tick tock tick tock tick tock
+
+// I've only got four minutes to save the world
+timbaland.announce(timeRemaining).while(!world.saved)
