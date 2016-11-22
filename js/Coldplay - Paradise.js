bdec4925781412ada4bd2b79128ba92ba4effba5
Harshit Harchani <hharchani@users.noreply.github.com>
2016-11-22 21:03:08 +0530

    Create Coldplay - Paradise.js

---
+++ js/Coldplay - Paradise.js
@@ -0,0 +1,99 @@
+repeat3 = (s) => [s, s, s]
+
+when(
+    she.was(
+        typeof she == 'girl'                // When she was just a girl
+    )
+).then((she) => {
+    she.expected(world)                     // She expected the world
+});
+
+her = she;
+it = world;
+
+but(
+    it.flewAwayFrom(her.reach())            // But it flew away from her reach so
+).so(
+    she.ranAwayIn(her.sleep())              // She ran away in her sleep
+) && she.dreamedOf(                         // And dreamed of
+    repeat3(
+        repeat3('para')
+            .join('-')
+            .concat('dise')
+    ).join(', ')                            // Para-para-paradise, Para-para-paradise, Para-para-paradise
+).forEach(she.closeEyes().getTimes());      // Every time she closed her eyes
+
+when(
+    she.was(
+        justA(girl)                         // When she was just a girl
+    )
+).then((she) => {
+    she.expected(world)                     // She expected the world
+});
+
+but(
+    it.flewAwayFrom(her.reach())            // But it flew away from her reach
+).catch((bullets) => {
+    bullets.pipe(her.teeth())               // And the bullets catch in her teeth
+});
+
+life.goes(on), it.get(so(heavy));           // Life goes on, it gets so heavy
+wheel.break(butterfly)                      // The wheel breaks the butterfly
+tears.forEach((aTear) => aTear = waterfall) // Every tear a waterfall
+
+let nightTime = new Date();
+nightTime.setHours(22);
+
+setTimeout(() => {
+    let interval = setInterval(() => {
+        if (night instanceof StormyNight) {
+            clearInterval(interval);
+            she.eyes().close();             // In the night the stormy night she'll close her eyes
+            she.flyAway();                  // In the night the stormy night away she'd fly
+        }
+    }, 24 * 60 * 60 * 1000 /* check everyday untill StormyNight */);
+}, nightTime.getTime() - Date.now())
+
+&& dreamsTimeBuffer = [];
+
+for (dreams of repeat3(                     // And dreams of
+        repeat3('para')                     // Para-para-paradise
+            .join('-')                      // Para-para-paradise
+            .concat('dise')                 // Para-para-paradise
+    ).join(', ')) {
+    dreamsTimeBuffer.push('oh')
+}
+
+while(dreamsTimeBuffer.pop());              // Oh oh oh oh oh oh-oh-oh
+
+she.dream.of(                               // She'd dream of
+    repeat3(
+        repeat3('para')                     // Para-para-paradise
+            .join('-')                      // Para-para-paradise
+            .concat('dise')                 // Para-para-paradise
+    ).join(', ')
+).forEach(() => {
+    dreamsTimeBuffer.push('oh')
+});
+
+while(buffer.pop())                         // Oh oh oh oh oh oh-oh-oh-oh
+
+&& lying({                                  // And so lying underneath those stormy skies
+    where: stormy(skies).underneath(),
+});
+
+she.say(
+    'oh, ' + repeat3('oh') + 'oh',
+    I.know(
+        @must
+        sun.set(rise)                        // She'd say, "oh, ohohohoh I know the sun must set to rise"
+    )
+)
+repeat3(
+    this.couldBe(                            // This could be
+        repeat3(                             // Para-para-paradise
+            repeat3('para')                  // Para-para-paradise
+                .join('-')                   // Para-para-paradise
+                .concat('dise')
+            ).join(', '))
+).map(() => repeat3(repeat3('oh')))          // Oh oh oh oh oh oh-oh-oh
