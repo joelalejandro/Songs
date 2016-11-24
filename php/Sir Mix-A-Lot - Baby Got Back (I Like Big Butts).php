07be63459433aaccc51bad79277fec0440ee5c20
Mridul Gupta <mridulg@users.noreply.github.com>
2016-11-24 14:31:24 +0530

    Sir Mix-A-Lot - Baby Got Back

---
+++ php/Sir Mix-A-Lot - Baby Got Back (I Like Big Butts).php
@@ -0,0 +1,80 @@
+<?php
+
+$i = new Person("Sir Mix A Lot");
+$i->likes = ["big butts"];
+$i->cannot_lie = true;
+$i->wants = [];
+
+$you = new Person;
+
+if(in_array("big butts", $i->likes) && $i->cannot_lie)
+{
+    $you = new Person("Other Brothers");   
+    $you->cannot("deny");
+
+
+    $girl = new Person;
+    $girl->waist = "itty bitty";
+
+
+    $i->face = ["round thing"];
+
+    while($girl->walks("in") && $girl->waist == "itty bitty" && in_array("round thing", $i->face))
+    {
+        $you->sprung();
+    }
+
+    if($girl->butt == "stuffed" && $you->notice())
+    {
+        $you->wants = ["pull up tough"];
+        $girl->pants = new Jeans;
+        $girl->pants->depth = "deep";
+
+        $i->hooked = true;
+        $i->eyes = new Eyes;
+        $i->eyes->focus($girl->butt);
+    }
+
+    // Oh, baby
+    $i->wants[] = "to get with ya";
+    $i->wants[] = "take your picture.";
+
+    $homeboys = new PersonGroup;
+    $homeboys->warn($i) or die("But that butt you got make me so horney");
+
+    // Ooh
+    $girl->butt = "smooth skin";
+    $girl->wants[] = "get in my benz";
+
+    $girl->use($i)->use($i);
+    $girl->groupy = "not average";
+
+    $romancing = true;
+
+    $girls = new PersonGroup();
+    $i->eyes->focus($girls);
+    if($i->eyes->focus() == $girls && $girls->are("dancing"))
+    {
+        unset($romancing);
+    }
+
+    $girl->sweat(true);
+    $girl->wet(true);   
+    $girl->it = "going like a turbo vette";
+
+
+    if(Magazines()->say("flat butts are the thing"))
+    {
+        $i->tired();
+    }
+
+    People()->average(['race' => 'black', 'gender' => 'male'])->ask("flat butts" == "the thing");
+
+    shake("it");
+    shake("it");
+    shake("it");
+    shake("that healthy butt");
+
+    $girl->nickname = "Baby";
+    $girl->got("back");
+}
